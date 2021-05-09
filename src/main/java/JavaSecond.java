9a8633e75302c7248898cd89fb1b6713f1addc6c
Leonid Ermakovich <lermakovich@inbox.ru>
2021-05-10 02:35:03 +0300

    второе задание

--- src/main/java/JavaSecond.java
+++ src/main/java/JavaSecond.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class JavaSecond {
+import java.util.Scanner;
+
+public class JavaSecond {
+
+    public static void main(String[] args) {
+        JavaSecond jl = new JavaSecond ();
+        Scanner scanner = new Scanner (System.in);
+        System.out.println ("Введите аргументы");
+        int text = scanner.nextInt ();
+        System.out.println ("Зеркальное отображение " + jl.mirror (text));
+
+    }
+
+    public String mirror(int x) {
+        StringBuilder newStr = new StringBuilder (String.valueOf (x));
+        return newStr.reverse ().toString ();
+    }
 }
